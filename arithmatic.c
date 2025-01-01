07f8c3a70e7058b64255a095b09d39356eef7871
Mr-A12k <kabil448446@gmail.com>
2025-01-01 15:46:59 +0530

    compile txt added

---
+++ arithmatic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,k;
+
+    //ADDITOn
+    printf("Enter two values for Addition:\n");
+    scanf("%d %d",&i,&j);
+    k=i+j;
+    printf("the addition of these numbers is: %d", k);
+
+
+    return 1;
+}
