d2f7bbf5271d6bd8195af91ff1444c16fe59cce5
Mr-A12k <kabil448446@gmail.com>
2025-01-01 15:14:53 +0530

    user input

---
+++ userinput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    int i,j,k;
+
+    printf("Enter two values: \n");
+    scanf("%d",&i);
+    scanf("%d",&j);
+    k=i+j;
+
+    printf("The value of k is %d \n",k);
+}
